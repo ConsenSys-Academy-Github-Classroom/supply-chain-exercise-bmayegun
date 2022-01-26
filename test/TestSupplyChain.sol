87daf92f36315c319efc78ab035348b26ca2679d
Bisoye <bmayegun@gmail.com>
2022-01-26 18:54:35 +0100

    Please work again

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.16 <0.9.0;
+pragma solidity ^0.8.0;
 
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
